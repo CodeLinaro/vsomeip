31737d4f0a661286363070d1c9ab833eb0319396
RajendranS <rajsub@qti.qualcomm.com>
2025-11-07 08:10:11 +0530

    Revert "Support for e2e header striping based on configuration for profile4 (experimental)."
    This reverts commit 3b41126fa2c12666fd23ff3073106e9cd9b328f6.

--- implementation/routing/include/routing_manager_impl.hpp
+++ implementation/routing/include/routing_manager_impl.hpp
@@ -558,8 +558,6 @@ private:
     std::mutex update_remote_subscription_mutex_;
 
     message_acceptance_handler_t message_acceptance_handler_;
-
-    std::vector<byte_t> edata_;
 };
 
 }  // namespace vsomeip_v3
